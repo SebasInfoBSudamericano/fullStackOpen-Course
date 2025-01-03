a1bcaf17385185de0b0b5d7e56b40e3592b7e176
SebasInfoBSudamericano <166545739+SebasInfoBSudamericano@users.noreply.github.com>
2025-01-03 13:17:04 -0500

    Added Content Part 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -15,7 +15,7 @@ const Content = ({ parts }) => {
   )
 }
 
-const Header = ({ heading }) => <h1>{heading}</h1>
+const Header = ({ heading }) => <h2>{heading}</h2>
 
 const Course = ({ course }) => {
   return (
@@ -38,34 +38,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <>
+      <Header heading={'Web development curriculum'}/>
+      <Course course={courses[0]}/>
+      <Course course={courses[1]}/>
+    </>
+  )
 }
-
 export default App
