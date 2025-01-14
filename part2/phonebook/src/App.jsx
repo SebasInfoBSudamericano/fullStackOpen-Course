35a1c30d677c84c55e5c44b3e08047c446f8cc80
Sebastian Munzon <166545739+SebasInfoBSudamericano@users.noreply.github.com>
2025-01-13 20:08:57 -0500

    Added Content Part 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,41 @@
+import { use } from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [newName, setNewName] = useState('')
   const handleName = (e) => {
-    console.log('value -> ' + e.target.value)
+    console.log('name -> ' + e.target.value)
     setNewName(e.target.value)
   }
 
+  const [newNumber, setNewNumber] = useState('')
   const handleNumber = (e) => {
-    console.log('value -> ' + e.target.value)
+    console.log('number -> ' + e.target.value)
     setNewNumber(e.target.value)
   }
+
+  const [filter, setFilter] = useState('')
+  const handleFilter = (e) => {
+    console.log('filter -> ' + e.target.value)
+    setFilter(e.target.value)
+  }
+
+  const toShow = filter == ''
+    ? persons
+      .map(person => <p key={person.id}>{person.name} {person.number}</p>)
+    : persons
+      .filter(person => 
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+      .map(person => <p key={person.id}>{person.name} {person.number}</p>)
 
   const handleSumbit = (e) => {
     e.preventDefault()
@@ -23,9 +43,11 @@ const App = () => {
       name: newName,
       number: newNumber
     }
+
     const alreadyAdded = persons.find(person => person.name == personObject.name || person.number == personObject.number)
 
-    alreadyAdded ? alert(
+    alreadyAdded
+      ? alert(
         alreadyAdded.name === personObject.name
           ? `${newName} is already added to the phonebook`
           : `${newNumber} is already added to the phonebook`
@@ -38,22 +60,21 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={filter} onChange={handleFilter} />
+      <h2>add a new</h2>
       <form onSubmit={handleSumbit}>
         <div>
           name: <input value={newName} onChange={handleName} required />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumber} pattern="[0-9+\(\)\- ]*" required/>
+          number: <input value={newNumber} onChange={handleNumber} pattern="[0-9+\(\)\- ]*" required />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => {
-        console.log(person.name)
-        return <p key={index}>{person.name} {person.number}</p>
-      })}
+      {toShow}
     </div>
   )
 }
